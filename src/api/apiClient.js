b1efd23c46d21afd8ec52a2c5145ca0538538f67
Rohit Dhyani <rohitdhyani50@gmail.com>
2025-09-19 18:53:24 +0530

    Update backend API to production URL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://smart-todo-backend-o760.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
