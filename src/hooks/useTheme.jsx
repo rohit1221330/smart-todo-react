6265239dc2e0a7fef8e86b37c773aebccd97389f
Rohit Dhyani <rohitdhyani50@gmail.com>
2025-09-20 09:30:49 +0530

    Update Dark Theme

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'Dark');
 
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
